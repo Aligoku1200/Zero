1327324b5816cfc28215282ef1b3b05a82ac7fd0
Aligoku1200 <106703834+Aligoku1200@users.noreply.github.com>
2025-07-29 11:13:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import os
 os.system("pip install amino.li")
 import aminoli
-Ç=aminoli.Client()
+Ç=aminoli.Client(deviceId="199E7E05ED4170454E540FA065B5F884765EEC1C58832A77BEABB77FE52664C5C8153802C4607D6CDA")
 E="zxvo@digdig.org"
 P="MiRA##"
 Ç.login(E,P)
+print(999999999999999)
