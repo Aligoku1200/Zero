c41b25c91f30e523a405d2b0e1a953ff00e82eb3
Aligoku1200 <106703834+Aligoku1200@users.noreply.github.com>
2025-07-29 13:15:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
-os.system("pip install amino.li")
-import aminoli
-Ç=aminoli.Client(deviceId="199E7E05ED4170454E540FA065B5F884765EEC1C58832A77BEABB77FE52664C5C8153802C4607D6CDA")
+os.system("pip install amino.fix")
+import aminofix
+Ç=aminofix.Client(deviceId="199E7E05ED4170454E540FA065B5F884765EEC1C58832A77BEABB77FE52664C5C8153802C4607D6CDA")
 E="zxvo@digdig.org"
 P="MiRA##"
 Ç.login(E,P)
