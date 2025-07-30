a2d845f27371f58b82b087c2957519a75d16f34e
Aligoku1200 <106703834+Aligoku1200@users.noreply.github.com>
2025-07-30 07:49:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import os
-os.system("pip install amino.fix==2.3.6.1")
-import aminofix
-Ç=aminofix.Client()
-Ç.login(
-"zxvo@digdig.org",
-"MiRA##")
+os.system("pip install amino.li")
+import aminoli as A
+Ç=A.Client()
+E="zxvo@digdig.org"
+P="MiRA##"
+E1="xzv@usbc.be"
+P1="GOKU12"
+Ç.login(E1,P1)
 L=Ç.get_from_code("http://aminoapps.com/p/v3r9m7")
 C=L.comId
 O=L.objectId
-S=aminofix.SubClient(comId=C,profile=Ç.profile)
+S=A.SubClient(comId=C,profile=Ç.profile)
+S.join_chat(O)
 S.send_message(chatId=O,message="99")
 print(999999999999999)
